3febdf9d043ea7afaabff7182f53896defdf5515
luoochen <jiu-mu@outlook.com>
2021-04-08 21:39:42 +0800

    add solution

--- leetcode/LCCUP2021/02/Solution.java
+++ leetcode/LCCUP2021/02/Solution.java
@@ -1,27 +1,52 @@
 class Solution {
     public int orchestraLayout(int num, int xPos, int yPos) {
-        int n = num*num;
-        int xMax = num;
-        int yMax = num;
-        int x = 0;
+        int yMin = 0;
+        int yMax = num-1;
+        int xMin = 0;
+        int xMax = num-1;
         int y = 0;
+        int x = 0;
         int cur = 1;
-        for (int i = 0; i < xMax; i++) {
-            for (int j = 0; j < yMax; j++) {
 
+        while (true) {
+            if (x == xPos && y == yPos) return cur;
+            if (x == xMin && y < yMax) {
+                // top row ->
+                // when is not first row
+                if (xMin > yMin) {
+                    yMin++;
+                }
+                y++;
+            } else if (y == yMax && x < xMax) {
+                // right column
+                if (yMin == xMin) {
+                    xMin++;
+                }
+                x++;
+            } else if (x == xMax && y > yMin) {
+                // bottom row <-
+                if (xMax == yMax) {
+                    yMax--;
+                }
+                y--;
+            } else if (y == yMin && x > xMin) {
+                // left column
+                if (yMax < xMax) {
+                    xMax--;
+                }
+                x--;
             }
-            System.out.println(cur+" ");
-            if (cur == 9) {
-                cur = 1;
-            } else {
-                cur++;
-            }
+            cur++;
+            if (cur != 9) cur = cur % 9;
         }
-        return 1;
     }
 
     public static void main(String[] args) {
 
-        // Solution sol = new Solution();
+        int num = 3;
+        int xPos = 0;
+        int yPos = 2;
+        Solution sol = new Solution();
+        System.out.println("Current index: " + sol.orchestraLayout(num, xPos, yPos));
     }
 }
