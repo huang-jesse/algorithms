4694e767baec6bb580103dd7a77d30c8a272dbc2
luoochen <jiu-mu@outlook.com>
2021-05-16 22:56:07 +0800

    feat: add leetcode biweekly-contest-52/04

--- leetcode/biweekly-contest-52/04/Solution.java
+++ leetcode/biweekly-contest-52/04/Solution.java
@@ -1,34 +1,49 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 class Solution {
     static final int MOD = (int)1e9 + 7;
     public int sumOfFlooredPairs(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
         int ans = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int cur = nums[i];
-            if (map.containsKey(cur)) {
-                ans = (ans + map.get(cur)) % MOD;
-            } else {
-                int temp = 0;
-                int j = 0;
-                while (j < nums.length) {
-                    int curNum = nums[j];
-                    int val = cur / curNum;
-                    temp = (temp + val) % MOD;
-                    j++;
-                }
-                map.put(cur, temp);
-                ans = (ans + temp) % MOD;
+        TreeMap<Integer, Integer> countMap = new TreeMap<>();
+        Map<Integer, Integer> endIndexMap = new HashMap<>();
+        for (int i = 0; i< n; i++) {
+            endIndexMap.put(nums[i], i);
+            countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
             }
+            int target = nums[i];
+            int count = countMap.get(target);
+            int tempSum = 0;
+            int j = i;
+            while (j < n) {
+                int cur = nums[j];
+                int quotient = cur / target;
+                int nextLimit = target * (quotient + 1);
+                int next = countMap.floorKey(nextLimit-1);
+                int endIndex = endIndexMap.get(next);
+                int range = endIndex - j + 1;
+                tempSum = (int)(((long)tempSum + ((long)count * (long)quotient * (long)range)) % MOD);
+
+                j = endIndex + 1;
+            }
+            ans = (ans + tempSum) % MOD;
         }
         return ans;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] nums = new int[]{2,5,9};
+        int[] nums = new int[]{7,7,7,7,7,7,7};
+        // int[] nums = new int[]{4,1,1,2,4};
         System.out.println("test: " + sol.sumOfFlooredPairs(nums));
     }
 }
