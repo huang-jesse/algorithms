426b4efdff9407154d338ac967c753d3ff72d506
luoochen <jiu-mu@outlook.com>
2021-07-07 20:55:56 +0800

    feat: add leetcode 1711

---
+++ leetcode/1711/Solution.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int countPairs(int[] deliciousness) {
+        final int MOD = 1000000007;
+        int maxVal = 0;
+        for (int val : deliciousness) {
+            maxVal = Math.max(maxVal, val);
+        }
+        int maxSum = maxVal * 2;
+        int pairs = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = deliciousness.length;
+        for (int i = 0; i < n; i++) {
+            int val = deliciousness[i];
+            for (int sum = 1; sum <= maxSum; sum <<= 1) {
+                int count = map.getOrDefault(sum - val, 0);
+                pairs = (pairs + count) % MOD;
+            }
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] deliciousness = {1,1,1,3,3,3,7};
+        System.out.println("test: " + sol.countPairs(deliciousness));
+    }
+}
