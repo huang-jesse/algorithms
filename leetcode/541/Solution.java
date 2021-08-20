f8ea55fd8f7d75756f0cc56794f572ff88a2448b
luoochen <jiu-mu@outlook.com>
2021-08-20 23:42:57 +0800

    feat: add leetcode 541

---
+++ leetcode/541/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(arr, i, Math.min(i + k, n) - 1);
+        }
+        return new String(arr);
+    }
+
+    public void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "abcdefg";
+        int k = 2;
+        System.out.println("test: " + sol.reverseStr(s, k));
+    }
+}
