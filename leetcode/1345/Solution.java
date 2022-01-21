2fda84b70772922a1460d510f56135a443507308
luoochen <jiu-mu@outlook.com>
2022-01-22 00:00:04 +0800

    feat: add leetcode 1345

---
+++ leetcode/1345/Solution.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+class Solution {
+    public int minJumps(int[] arr) {
+        Map<Integer, List<Integer>> idxSameValue = new HashMap<Integer, List<Integer>>();
+        for (int i = 0; i < arr.length; i++) {
+            idxSameValue.putIfAbsent(arr[i], new ArrayList<Integer>());
+            idxSameValue.get(arr[i]).add(i);
+        }
+        Set<Integer> visitedIndex = new HashSet<Integer>();
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[]{0, 0});
+        visitedIndex.add(0);
+        while (!queue.isEmpty()) {
+            int[] idxStep = queue.poll();
+            int idx = idxStep[0], step = idxStep[1];
+            if (idx == arr.length - 1) {
+                return step;
+            }
+            int v = arr[idx];
+            step++;
+            if (idxSameValue.containsKey(v)) {
+                for (int i : idxSameValue.get(v)) {
+                    if (visitedIndex.add(i)) {
+                        queue.offer(new int[]{i, step});
+                    }
+                }
+                idxSameValue.remove(v);
+            }
+            if (idx + 1 < arr.length && visitedIndex.add(idx + 1)) {
+                queue.offer(new int[]{idx + 1, step});
+            }
+            if (idx - 1 >= 0 && visitedIndex.add(idx - 1)) {
+                queue.offer(new int[]{idx - 1, step});
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        // int[] arr = {7,6,9,6,9,6,9,7};
+        int[] arr = {51,64,-15,64,-41,-15,-63};
+        // int[] arr = {11,22,7,7,7,7,7,7,7,22,13};
+        // int[] arr = {100,-23,-23,404,100,23,23,23,3,404};
+        System.out.println("test: " + sol.minJumps(arr));
+    }
+}
