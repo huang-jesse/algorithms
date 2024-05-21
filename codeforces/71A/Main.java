770b8d21eb459b9d5cbd5f73c4b253298ce07b30
huang-jesse <huang-jesse@outlook.com>
2024-05-21 15:27:50 +0800

    feat: add codeforeces

---
+++ codeforces/71A/Main.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Main {
+    static FastScanner in = new FastScanner();  // 快读参数
+    static PrintWriter out = new PrintWriter(System.out);   // 输出结果参数
+
+    /**
+     * 主逻辑
+     * **/
+    public static void solve() {
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String word = in.next();
+            int len = word.length();
+            if (len > 10) {
+                out.println(word.charAt(0) + String.valueOf(len - 2) + word.charAt(len - 1));
+            } else {
+                out.println(word);
+            }
+        }
+    }
+
+    /**
+     * 运行主函数
+     * T表示测试案例数量 多用于CodeForces 其他OJ需要根据情况进行修改
+     * */
+    public static void main(String[] args) {
+        // int T = in.nextInt();
+        // while (T-- > 0) {
+        //     solve();
+        // }
+        solve();
+        out.close();
+    }
+
+    /**
+     * 自定义类
+     * 需要重写hashCode方法和equals方法
+     * 这样才能正确使用Set<T>和Map<K, V>等集合元素（多用于哈希表）
+     * */
+    static class PII {
+        int x;
+        int y;
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PII p = (PII) o;
+            return Objects.equals(x, p.x) && Objects.equals(y, p.y);
+        }
+
+        PII(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    /**
+     * 快读模板
+     * */
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        // 读取字符串
+        String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        // 读取输入的数字
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+
+        // 读取整型数组
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long[] readLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+}
