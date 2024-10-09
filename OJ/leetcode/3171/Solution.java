defa75df6ec3034eb3d0ba2414c83dada6e246bc
huang-jesse <huang-jesse@outlook.com>
2024-10-09 11:29:05 +0800

    feat: add leetcode 3171

---
+++ OJ/leetcode/3171/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        int[] dpBitwise = new int[n];
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            ans = Math.min(ans, Math.abs(x - k));
+            dpBitwise[i] = x;
+            for (int j = i - 1; j >= 0 && (dpBitwise[j] | x) != dpBitwise[j]; j--) {
+                dpBitwise[j] = dpBitwise[j] | x;
+                ans = Math.min(ans, Math.abs(dpBitwise[j] - k));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1,2,4,5};
+        int k = 3; // res: 0
+        System.out.println("test: " + sol.minimumDifference(nums, k));
+    }
+}
